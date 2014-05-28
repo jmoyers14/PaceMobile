2afc69da06d30ea1ca37ba609fcef8cf8750824d
Jeremy Moyers <jmoyers14@gmail.com>
2014-05-28 15:57:01 -0700

    added empty edit account view.. no function for it

---
+++ TicketBuilder/EditAccountViewController.h
@@ -0,0 +1,14 @@
+//
+//  EditAccountViewController.h
+//  PaceMobile
+//
+//  Created by Jeremy Moyers on 5/28/14.
+//  Copyright (c) 2014 Bluesage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NetworkViewController.h"
+#import "PMUser.h"
+@interface EditAccountViewController : UITableViewController
+
+@end
